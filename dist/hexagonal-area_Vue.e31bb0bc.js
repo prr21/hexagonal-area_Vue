73aee358a34b4a63fc5c7059dd677431be324a4f
Kurban <Mr.gustav009@gmail.com>
2021-03-15 22:52:33 +0300

    table prod

--- dist/hexagonal-area_Vue.e31bb0bc.js
+++ dist/hexagonal-area_Vue.e31bb0bc.js
@@ -525,7 +525,7 @@ Vue.component("hexagonal", {
 });
 
 _app.default.renderDomens();
-},{"./src/app":"src/app.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./src/app":"src/app.js"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -553,7 +553,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50885" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49949" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -729,5 +729,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
 //# sourceMappingURL=/hexagonal-area_Vue.e31bb0bc.js.map
